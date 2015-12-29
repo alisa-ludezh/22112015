6b43cffe86829af06c3305b20394cdcb66bb0f41
Alisa Pugacheva <alisapugacheva@ya.ru>
2015-12-29 08:02:01 +0300

    UserControl

---
+++ WebApplication1/LoginMy.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginMy.aspx.cs" Inherits="WebApplication1.LoginMy" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Login runat="server" OnAuthenticate="Unnamed1_Authenticate" ID="Login1">
+
+        </asp:Login>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace WebApplication2
+{
+    public class Person
+    {
+        private string personFIO;
+        private string personUID;
+        private string Job;
+
+        public Person(
+            string personUID,
+            string personFIO,
+            string Job)
+        {
+            this.personUID = personUID;
+            this.personFIO = personFIO;
+            this.Job = Job;
+        }
+    }
+}
